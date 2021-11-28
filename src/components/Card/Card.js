2c93129bbadf17c691cd3bbc2529d7814218e587
pedro <pedro030197>
2021-11-28 12:24:54 -0300

    feat: include description, types, categorys and data search.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,10 +5,10 @@ export default function Card({content}) {
 
   return (
     <Box p="3" bg="yellow.100" m="20px" maxW="360px" color="black"  borderWidth="1px" borderRadius="15" > 
-      {content.tema1 }
+      {content.description }
       <Box display='flex' justifyContent='space-between' p="3">
           <Box as="span" color="gray.600" fontSize="sm">
-            {content.portal }
+            {content.name }
           </Box>
           <Box as="span" color="gray.600" fontSize="sm">
             Acessar a página <ArrowForwardIcon/>
